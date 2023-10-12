731bb4469e6d80c03cbde3810ab9b65e21564703
Christian Alquisada <calquisada@aol.com>
2023-10-11 20:30:31 -0700

    sending index and show data

---
+++ server.js
@@ -0,0 +1,21 @@
+// dependencies
+const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+const breadsController = require('./controllers/breads_controllers.js')
+app.use('/breads', breadsController)
+
+// listen
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+})
+
