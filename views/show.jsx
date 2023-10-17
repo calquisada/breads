6fce760786b3921112fd3e6cc1fe0055916e406e
Christian Alquisada <calquisada@aol.com>
2023-10-16 20:42:03 -0700

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -1,5 +1,5 @@
 const React = require('react')
-const Default = require('./layouts/default')
+const Default = require('./layouts/Default')
 
 // function Show ({bread}) {
 //  console.log(bread.name)
@@ -19,6 +19,7 @@ function Show ({bread, index}) {
     <form action={`/breads/${index}?_method=DELETE`} method="POST">
     <input type='submit' value="DELETE"/>
     </form>
+    <a href={`/breads/${index}/edit`}><button>Edit</button></a>
     <p>
       and it
       {
