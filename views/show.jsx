40117426864427bfacf8a0c15319775a4ba5f9ab
Christian Alquisada <calquisada@aol.com>
2023-10-23 19:12:33 -0700

    commit

--- views/show.jsx
+++ views/show.jsx
@@ -16,10 +16,10 @@ function Show ({bread, index}) {
     return (
     <Default>
     <h3>{bread.name}</h3>
-    <form action={`/breads/${index}?_method=DELETE`} method="POST">
-    <input type='submit' value="DELETE"/>
+    <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
+      <input type='submit' value="DELETE"/>
     </form>
-    <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+    <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
     <p>
       and it
       {
